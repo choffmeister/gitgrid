77294a583adb5fec7f73368509b307c74039e0f9
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-03-17 19:22:08 +0100

    Add API routes to travers git repositories

--- src/main/scala/com/gitgrid/http/ApiHttpServiceActor.scala
+++ src/main/scala/com/gitgrid/http/ApiHttpServiceActor.scala
@@ -3,7 +3,7 @@ package com.gitgrid.http
 import akka.actor._
 import com.gitgrid.Config
 import com.gitgrid.auth._
-import com.gitgrid.git.GitRepository
+import com.gitgrid.git._
 import com.gitgrid.http.directives._
 import com.gitgrid.managers.UserManager
 import com.gitgrid.models._
@@ -44,7 +44,62 @@ class ApiHttpServiceActor(val db: Database) extends Actor with ActorLogging with
 
   lazy val projectsRoute =
     projectPathPrefix { project =>
-      complete(project)
+      pathEnd {
+        complete(project)
+      } ~
+      pathPrefix("git") {
+        def gitRepository[T](project: Project)(inner: GitRepository => T): T =
+          GitRepository(new File(Config.repositoriesDir, project.id.get.stringify))(inner)
+
+        path("branches") {
+          complete(gitRepository(project)(repo => repo.branches()))
+        } ~
+        path("tags") {
+          complete(gitRepository(project)(repo => repo.tags()))
+        } ~
+        path("commits") {
+          complete(gitRepository(project)(repo => repo.commits()))
+        } ~
+        path("commit" / Segment) { refOrSha =>
+          complete(gitRepository(project)(repo => repo.commit(repo.resolve(refOrSha))))
+        } ~
+        path("tree" / Segment) { sha =>
+          complete(gitRepository(project)(repo => repo.tree(repo.resolve(sha))))
+        } ~
+        path("blob" / Segment) { sha =>
+          complete(gitRepository(project)(repo => repo.blob(repo.resolve(sha)).readAsString(repo)))
+        } ~
+        path("tree" / Segment / RestPath) { (refOrSha, path) =>
+          complete {
+            gitRepository(project) { repo =>
+              val commitId = repo.resolve(refOrSha)
+              val commit = repo.commit(commitId)
+              val tree = repo.traverse(commit, "/" + path).asInstanceOf[GitTree]
+              tree
+            }
+          }
+        } ~
+        path("blob" / Segment / RestPath) { (refOrSha, path) =>
+          complete {
+            gitRepository(project) { repo =>
+              val commitId = repo.resolve(refOrSha)
+              val commit = repo.commit(commitId)
+              val blob = repo.traverse(commit, "/" + path).asInstanceOf[GitBlob]
+              blob
+            }
+          }
+        } ~
+        path("blob-raw"/ Segment / RestPath) { (refOrSha, path) =>
+          complete {
+            gitRepository(project) { repo =>
+              val commitId = repo.resolve(refOrSha)
+              val commit = repo.commit(commitId)
+              val blob = repo.traverse(commit, "/" + path).asInstanceOf[GitBlob]
+              blob.readAsString(repo)
+            }
+          }
+        }
+      }
     } ~
     pathEnd {
       post {
