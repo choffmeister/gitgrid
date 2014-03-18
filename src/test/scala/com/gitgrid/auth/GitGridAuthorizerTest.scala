760c74428bb5650e3ac1174a7b2d774e00aa32ac
Christian Hoffmeister <mail@choffmeister.de>
2014-03-18 21:04:06 +0100

    Add test for GitGridAuthorizer

--- src/test/scala/com/gitgrid/auth/GitGridAuthorizerTest.scala
+++ src/test/scala/com/gitgrid/auth/GitGridAuthorizerTest.scala
@@ -5,10 +5,26 @@ import org.specs2.mutable._
 
 class GitGridAuthorizerTest extends Specification {
   "GitGridAuthorizer" should {
-    "work" in new TestDatabase {
+    "properly grant and deny access to projects" in new TestDatabase {
       val auth = new GitGridAuthorizer(db)
 
-      skipped("Inconclusive")
+      await(auth.authorize(None, ProjectRead(project1))) === false
+      await(auth.authorize(None, ProjectRead(project2))) === false
+      await(auth.authorize(Some(user2), ProjectRead(project1))) === false
+      await(auth.authorize(Some(user1), ProjectRead(project2))) === false
+      await(auth.authorize(Some(user1), ProjectRead(project1))) === true
+      await(auth.authorize(Some(user2), ProjectRead(project2))) === true
+    }
+
+    "properly grant and deny access to project repositores" in new TestDatabase {
+      val auth = new GitGridAuthorizer(db)
+
+      await(auth.authorize(None, ProjectRepositoryReadWrite(project1))) === false
+      await(auth.authorize(None, ProjectRepositoryReadWrite(project2))) === false
+      await(auth.authorize(Some(user2), ProjectRepositoryReadWrite(project1))) === false
+      await(auth.authorize(Some(user1), ProjectRepositoryReadWrite(project2))) === false
+      await(auth.authorize(Some(user1), ProjectRepositoryReadWrite(project1))) === true
+      await(auth.authorize(Some(user2), ProjectRepositoryReadWrite(project2))) === true
     }
   }
 }
