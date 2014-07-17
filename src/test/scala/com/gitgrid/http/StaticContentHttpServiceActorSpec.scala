a7913ecf493ae4a29def026f528afdebcfbe2fbc
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-07-17 21:55:51 +0200

    Improve StaticContentHttpServiceActorSpec
    Check for the response content.

--- src/test/scala/com/gitgrid/http/StaticContentHttpServiceActorSpec.scala
+++ src/test/scala/com/gitgrid/http/StaticContentHttpServiceActorSpec.scala
@@ -25,10 +25,12 @@ class StaticContentHttpServiceActorSpec extends Specification with AsyncUtils {
       val req1 = HttpRequest(method = GET, uri = Uri("/"))
       val res1 = await(httpService ? req1).asInstanceOf[HttpResponse]
       res1.status === OK
+      res1.entity.asString must contain("<title>GitGrid</title>")
 
       val req2 = HttpRequest(method = GET, uri = Uri("/index.html"))
       val res2 = await(httpService ? req2).asInstanceOf[HttpResponse]
       res2.status === OK
+      res2.entity.asString must contain("<title>GitGrid</title>")
     }
 
     "serve static content" in new TestActorSystem with TestEnvironment {
@@ -38,6 +40,7 @@ class StaticContentHttpServiceActorSpec extends Specification with AsyncUtils {
       val req1 = HttpRequest(method = GET, uri = Uri("/scripts/app.js"))
       val res1 = await(httpService ? req1).asInstanceOf[HttpResponse]
       res1.status === OK
+      res1.entity.asString must contain("console.log('app')");
     }
   }
 }
