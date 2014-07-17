d57ee16ff6ab1192f6379a58526893cfda04b6f4
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-07-17 20:50:43 +0200

    Update sbt-pack to v0.6.0
    Replace manual content copying on sbt-pack by configuration based
    inclusion.

--- src/main/scala/com/gitgrid/http/StaticContentHttpServiceActor.scala
+++ src/main/scala/com/gitgrid/http/StaticContentHttpServiceActor.scala
@@ -1,17 +1,21 @@
 package com.gitgrid.http
 
 import akka.actor._
+import com.gitgrid.Config
 import spray.http._
 import spray.routing._
 
-class StaticContentHttpServiceActor extends Actor with HttpService {
+class StaticContentHttpServiceActor(cfg: Config) extends Actor with HttpService {
   implicit val actorRefFactory = context
   implicit val executor = context.dispatcher
   implicit val timeout = akka.util.Timeout(1000)
 
   def receive = runRoute(route)
-  def route = path(Rest) {
-    case filePath if filePath.length > 0 => getFromResource("web/" + filePath)
-    case _ => getFromResource("web/index.html")
+  def route = cfg.webDir.map(_.toString) match {
+    case Some(webDir) =>
+      pathSingleSlash(getFromFile(s"${webDir}/index.html")) ~
+      getFromDirectory(webDir)
+    case _ =>
+      reject()
   }
 }
