42417da95715f0fc22c108b1f74dcf6bc0cddd94
Christian Hoffmeister <mail@choffmeister.de>
2014-07-25 19:26:53 +0200

    Filter HTTP basic challenge headers

--- src/main/scala/com/gitgrid/auth/GitGridHttpAuthenticator.scala
+++ src/main/scala/com/gitgrid/auth/GitGridHttpAuthenticator.scala
@@ -4,6 +4,8 @@ import com.gitgrid.Config
 import com.gitgrid.managers._
 import com.gitgrid.models._
 import com.gitgrid.http.JsonProtocol
+import spray.http.HttpChallenge
+import spray.http.HttpHeaders.`WWW-Authenticate`
 import scala.concurrent._
 import spray.routing._
 import spray.routing.authentication._
@@ -15,5 +17,20 @@ class GitGridHttpAuthenticator(cfg: Config, db: Database)(implicit executionCont
   val basicAuthenticator = new EnhancedBasicHttpAuthenticator[User](cfg.httpAuthRealm, userPassAuthenticator)
   val authenticator = EnhancedHttpAuthenticator.combine(bearerTokenAuthenticator, basicAuthenticator)
 
-  def apply(ctx: RequestContext): Future[Authentication[User]] = authenticator(ctx)
+  def apply(ctx: RequestContext): Future[Authentication[User]] = authenticator(ctx).map {
+    // filter out HTTP basic challenges to prevent interactive window in browsers
+    case Left(AuthenticationFailedRejection(c, ch)) =>
+      Left(AuthenticationFailedRejection(c, ch.map {
+        case `WWW-Authenticate`(ch) =>
+          `WWW-Authenticate`(ch.filter {
+            case HttpChallenge("Basic", _, _) => false
+            case _ => true
+          })
+        case x => x
+      } filter {
+        case `WWW-Authenticate`(Nil) => false
+        case _ => true
+      }))
+    case x => x
+  }
 }
