038d495f716464e02a1f0d3f9eb96a61d293addf
Christian Hoffmeister <hoffmeister@mailbox.org>
2014-07-27 14:30:27 +0200

    Code clean up

--- src/test/scala/com/gitgrid/http/ApiHttpServiceActorSpec.scala
+++ src/test/scala/com/gitgrid/http/ApiHttpServiceActorSpec.scala
@@ -252,9 +252,9 @@ class ApiHttpServiceActorSpec extends Specification with Specs2RouteTest with As
   "ApiHttpServiceActor handle bearer tokens" should {
     "accept a valid bearer token" in new TestApiHttpService {
       Post("/api/auth/login", AuthenticationRequest("user1", "pass1")) ~> route ~> check {
-        val token = responseAs[AuthenticationResponse].token.get
+        val tokenStr = responseAs[AuthenticationResponse].token.get
 
-        Get("/api/auth/state") ~> addHeader("Authorization", "Bearer " + token) ~> route ~> check {
+        Get("/api/auth/state") ~> addHeader(`Authorization`(OAuth2BearerToken(tokenStr))) ~> route ~> check {
           status === OK
           responseAs[User].userName == "user1"
         }
@@ -263,9 +263,9 @@ class ApiHttpServiceActorSpec extends Specification with Specs2RouteTest with As
 
     "reject an expired bearer token" in new TestApiHttpService {
       Post("/api/auth/login", AuthenticationRequest("user1", "pass1", Some(new Date(System.currentTimeMillis - 1)))) ~> route ~> check {
-        val token = responseAs[AuthenticationResponse].token.get
+        val tokenStr = responseAs[AuthenticationResponse].token.get
 
-        Get("/api/auth/state") ~> addHeader("Authorization", "Bearer " + token) ~> sealedRoute ~> check {
+        Get("/api/auth/state") ~> addHeader(`Authorization`(OAuth2BearerToken(tokenStr))) ~> sealedRoute ~> check {
           status === Unauthorized
           val authenticateHeader = header[`WWW-Authenticate`]
           authenticateHeader must beSome
@@ -278,10 +278,10 @@ class ApiHttpServiceActorSpec extends Specification with Specs2RouteTest with As
       Post("/api/auth/login", AuthenticationRequest("user1", "pass1", Some(new Date(System.currentTimeMillis - 1)))) ~> route ~> check {
         val tokenStr = responseAs[AuthenticationResponse].token.get
 
-        Get("/api/auth/renew") ~> addHeader("Authorization", "Bearer " + tokenStr) ~> route ~> check {
+        Get("/api/auth/renew") ~> addHeader(`Authorization`(OAuth2BearerToken(tokenStr))) ~> route ~> check {
           val token2Str = responseAs[AuthenticationResponse].token.get
 
-          Get("/api/auth/state") ~> addHeader("Authorization", "Bearer " + token2Str) ~> route ~> check {
+          Get("/api/auth/state") ~> addHeader(`Authorization`(OAuth2BearerToken(token2Str))) ~> route ~> check {
             status === OK
           }
         }
